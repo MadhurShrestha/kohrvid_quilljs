18ff4efb304a81071e66325436866b23b9ad34fe
Aayush <Aayushstha25@gmail.com>
2022-03-01 16:54:59 +0545

    Working quill js editor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,14 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import "@hotwired/turbo-rails";
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "./quill-editor.js"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+
+// $(document).on("turbo:load", () => {
+// });
